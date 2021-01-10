f418098a5950efc3fb047d6f6f10ab4a99eb4acb
Roei Rom <roeirom6@gmail.com>
2021-01-10 12:50:25 +0200

    feat(index.ts): add dummy http server for azure deployment

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,45 @@
+import express from 'express';
 import { CronJob } from 'cron';
 
 import logger from './Logger';
 import { Severity } from './Logger/types';
 import { deleteOldInvestigations } from './DAL';
 
-new CronJob(process.env.CRON_JOB_TIMING, async () => {
-    try {
-        logger.info({
-            step: 'starting CRON job',
-            severity: Severity.LOW
-        });
-        await deleteOldInvestigations();
-        logger.info({
-            step: 'finished CRON job',
-            severity: Severity.LOW
-        });
-    } catch (err) {
-        logger.error({
-            step: `error in deleteOldInestigation function: ${err}`,
-            severity: Severity.HIGH
-        })
-    }
-}).start();
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from investigation delete service!');
+})
+
+app.listen(+process.env.PORT, () => {
+
+    new CronJob(process.env.CRON_JOB_TIMING, async () => {
+        try {
+            logger.info({
+                step: 'starting CRON job',
+                severity: Severity.LOW
+            });
+            await deleteOldInvestigations();
+            logger.info({
+                step: 'finished CRON job',
+                severity: Severity.LOW
+            });
+        } catch (err) {
+            logger.error({
+                step: `error in deleteOldInestigation function: ${err}`,
+                severity: Severity.HIGH
+            })
+        }
+    }).start();
+
+    logger.info({
+        step: `Started Dummy HTTP server on port ${process.env.PORT}`,
+        severity: Severity.LOW
+    });
+    
+}).on('error', (err) => {
+    logger.error({
+        step: `Failed to setup Dummy HTTP server: ${err}`,
+        severity: Severity.CRITICAL
+    })
+})
