fc41cbab6f55056604a37f8eabdb209a657e7253
Roei Rom <52863381+RoeiRom@users.noreply.github.com>
2021-01-11 09:56:28 +0000

    fix(deleteInvestigation.sql): fix the delete investigation script (#9)

--- deleteInvestigation.sql
+++ deleteInvestigation.sql
@@ -14,49 +14,46 @@ declare
 	investigations_in_group int4;
 	group_id_to_inspect uuid;
 begin
-
+	
 	DELETE FROM public.investigated_patient_symptoms 
 		where investigation_id = delete_epi_number;
-
+	
 	DELETE FROM public.exposure 
 		where investigation_id = delete_epi_number;
-
-	DELETE FROM public.contact_event
-		where investigation_id = delete_epi_number;
-
-	-- deleting persons that are related to contact_event
-	DELETE FROM public.person
-		where id in (select person_info from public.contacted_person where contact_event in (select id from public.contact_event where investigation_id = delete_epi_number));
-
+	
 	DELETE FROM public.contacted_person  
 		where contact_event	in (select id from public.contact_event where investigation_id = delete_epi_number);
+		
+	DELETE FROM public.investigated_patient_background_diseases
+		WHERE investigated_patient_id in (select id from public.investigated_patient where covid_patient = delete_epi_number);
+	
+	DELETE FROM public.investigation_settings
+		WHERE epidemiology_number = delete_epi_number;
+	
+	DELETE FROM public.involved_contact
+		WHERE investigation_id = delete_epi_number;
+	
+	DELETE FROM public.contact_event
+		where investigation_id = delete_epi_number;
 		
 	-- deleting persons from involved contacts
 	DELETE FROM public.person
 		WHERE id in (select person_id from public.involved_contact where investigation_id = delete_epi_number);
-
-	DELETE FROM public.involved_contact
-		WHERE investigation_id = delete_epi_number;
-	
-	DELETE FROM public.investigation_settings
-		WHERE epidemiology_number = delete_epi_number;
-
-	DELETE FROM public.investigated_patient_background_diseases
-		WHERE investigated_patient_id in (select id from public.investigated_patient where covid_patient = delete_epi_number);
+		
+	-- deleting persons that are related to contact_event
+	DELETE FROM public.person
+		where id in (select person_info from public.contacted_person where contact_event in (select id from public.contact_event where investigation_id = delete_epi_number));
 		
 	SELECT INTO group_id_to_inspect
 	group_id
 	FROM investigation
 	WHERE epidemiology_number = delete_epi_number;
-
-    DELETE FROM public.investigation
+		
+	DELETE FROM public.investigation
 		WHERE epidemiology_number = delete_epi_number;
-
+		
 	DELETE FROM public.investigated_patient
 		where covid_patient = delete_epi_number;
-
-	DELETE FROM public.covid_patients
-		WHERE epidemiology_number = delete_epi_number;
 		
 	SELECT INTO investigations_in_group
 	COUNT(*)
